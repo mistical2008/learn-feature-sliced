dce6b61e451eea799542f7821480d5b6c7cc6a5a
Evgeniy Talko <etalko@gmail.com>
2022-03-01 21:09:00 +0300

    add: ui-kit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,10 @@
+import { UIKit as UI } from 'shared'
+
 const HomePage = () => {
     return (
         <div>
             Hello from <b>Home page</b>
+            <UI.Checkbox />
         </div>
     )
 }
